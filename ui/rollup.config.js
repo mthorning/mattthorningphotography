324f4bd9d2168d0b6f789abf69891148e054041f
Matt Thorning <matthewthorning@gmail.com>
2020-11-26 17:57:32 +0000

    fix missing PAYPAL_ID env var

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -6,15 +6,12 @@ import resolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import commonjs from '@rollup/plugin-commonjs'
 import svelte from 'rollup-plugin-svelte'
-// import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import config from 'sapper/config/rollup.js'
 import pkg from './package.json'
 
 const mode = process.env.NODE_ENV || 'development'
 const dev = mode === 'development'
-
-// const legacy = !!process.env.SAPPER_LEGACY_BUILD
 
 const onwarn = (warning, onwarn) =>
   (warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
@@ -45,32 +42,6 @@ export default {
         browser: true,
         dedupe: ['svelte'],
       }),
-      // commonjs(),
-
-      // legacy &&
-      //   babel({
-      //     extensions: ['.js', '.mjs', '.html', '.svelte'],
-      //     babelHelpers: 'runtime',
-      //     exclude: ['node_modules/@babel/**'],
-      //     presets: [
-      //       [
-      //         '@babel/preset-env',
-      //         {
-      //           targets: '> 0.25%, not dead',
-      //         },
-      //       ],
-      //     ],
-      //     plugins: [
-      //       '@babel/plugin-syntax-dynamic-import',
-      //       [
-      //         '@babel/plugin-transform-runtime',
-      //         {
-      //           useESModules: true,
-      //         },
-      //       ],
-      //     ],
-      //   }),
-
       !dev &&
         terser({
           module: true,
@@ -99,7 +70,6 @@ export default {
       resolve({
         dedupe: ['svelte'],
       }),
-      // commonjs(),
     ],
     external: Object.keys(pkg.dependencies).concat(
       require('module').builtinModules
